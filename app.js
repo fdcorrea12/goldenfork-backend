15e7062d67000ce797909e913c310c7595575077
Fabian Correa <becasfabian@hotmail.com>
2021-12-06 13:11:19 -0500

    backend init

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const ordenRoutes = require("./routes/orden.routes");
+
+// Import de las routes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// Configuracion de la conexion a la bd
+mongoose
+    .connect("mongodb+srv://dbGoldenFork:Goldenfork2021@cluster0.qle6c.mongodb.net/GoldenFork?retryWrites=true&w=majority")
+    //.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("Estamos conectados a nuestra BD");
+    })
+    .catch(() => {
+        console.log("Houston tenemos un problema");
+    });
+
+app.use("/api/orden", ordenRoutes);
+
+module.exports = app;
--- database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URL = 'mongodb+srv://dbGoldenFork:Goldenfork2021@cluster0.qle6c.mongodb.net/GoldenFork?retryWrites=true&w=majority';
-
-mongoose.connect(URL)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
-
-module.exports = mongoose;
