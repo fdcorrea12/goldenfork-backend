238d895441a48a1b526893a8f0f391243605944e
Fabian Correa <becasfabian@hotmail.com>
2021-12-06 13:37:09 -0500

    deploy BD_CONNECTION

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+
 const app = express();
 
 const ordenRoutes = require("./routes/orden.routes");
@@ -12,11 +13,12 @@ const ordenRoutes = require("./routes/orden.routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+require("dotenv").config();
 
 // Configuracion de la conexion a la bd
 mongoose
-    .connect("mongodb+srv://dbGoldenFork:Goldenfork2021@cluster0.qle6c.mongodb.net/GoldenFork?retryWrites=true&w=majority")
-    //.connect(process.env.DB_CONNECT)
+//.connect("mongodb+srv://dbGoldenFork:Goldenfork2021@cluster0.qle6c.mongodb.net/GoldenFork?retryWrites=true&w=majority")
+    .connect(process.env.DB_CONNECT)
     .then(() => {
         console.log("Estamos conectados a nuestra BD");
     })
